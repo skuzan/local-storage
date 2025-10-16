0cee20183beae420577db4718350d473daeb7daa
Sinan KUZAN <sinan_kuzan@hotmail.com>
2025-10-16 17:41:12 +0200

    Updated

--- local.js
+++ local.js
@@ -4,6 +4,18 @@ const clearBtn = document.querySelector(`#clear`);
 const list = document.getElementById(`list`);
 let key = 0;
 
+console.log(localStorage);
+
+document.addEventListener(`load`, () => {
+  for (let i = 0; i < localStorage.length; i++) {
+    const sKey = localStorage.key(i);
+    const SValue = localStorage.getItem(sKey);
+    const li = document.createElement("li");
+    li.textContent = `${sKey}. ${SValue}`;
+    list.appendChild(li);
+  }
+});
+
 addBtn.addEventListener(`click`, () => {
   const text = textInput.value.trim();
   localStorage.setItem(key + 1, text);
