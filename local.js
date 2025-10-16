48c9845d80572e43420769b115f923fef72df82c
Sinan KUZAN <sinan_kuzan@hotmail.com>
2025-10-16 23:06:45 +0200

    Updated

--- local.js
+++ local.js
@@ -2,31 +2,38 @@ const textInput = document.querySelector(`#text`);
 const addBtn = document.getElementById(`add`);
 const clearBtn = document.querySelector(`#clear`);
 const list = document.getElementById(`list`);
-let key = 0;
+let key = localStorage.setItem("key", 0);
+let key2 = Number(localStorage.getItem("key"));
 
-console.log(localStorage);
-
-document.addEventListener(`load`, () => {
+window.addEventListener(`load`, () => {
   for (let i = 0; i < localStorage.length; i++) {
     const sKey = localStorage.key(i);
     const SValue = localStorage.getItem(sKey);
-    const li = document.createElement("li");
-    li.textContent = `${sKey}. ${SValue}`;
-    list.appendChild(li);
+    if (key2 !== 0) {
+      const li = document.createElement("li");
+      li.textContent = `${sKey}. ${SValue}`;
+      list.appendChild(li);
+    }
   }
 });
 
 addBtn.addEventListener(`click`, () => {
   const text = textInput.value.trim();
-  localStorage.setItem(key + 1, text);
+  localStorage.setItem(key2 + 1, text);
   const li = document.createElement("li");
-  li.textContent = `${key + 1}. ${text}`;
+  li.textContent = `${key2 + 1}. ${text}`;
   list.appendChild(li);
-  key++;
+  key2++;
   textInput.value = "";
 });
 
 clearBtn.addEventListener(`click`, () => {
   localStorage.clear();
   list.innerHTML = "";
+});
+
+document.addEventListener(`keydown`, (e) => {
+  if (e.key.toUpperCase() === "ENTER") {
+    addBtn.click();
+  }
 });
